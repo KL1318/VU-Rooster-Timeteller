9e31d0104a29bb034a901426bc918e81d3d99baf
Ewout <242062643+KL1318@users.noreply.github.com>
2026-01-29 16:27:58 +0100

    feat: turned the date data into an object

--- contentscript.js
+++ contentscript.js
@@ -11,17 +11,20 @@ timeLine.style.height = "2px";
 
 let loadBar = setInterval(function applyChanges() {
     const date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let day = date.getDay() + 1;
-    let total_minutes = hours * 60 + minutes + 7;
+    const data = {
+        hour: date.getHours(),
+        minute: date.getMinutes(),
+        day: date.getDay() + 1
+    };
+    const total_minutes = data.hour * 60 + data.minute + 7;
+    // +7 because for some reason, the bar seems to be about 7 minutes off without it.
     let dayPercentage = total_minutes / MINUTES_IN_DAY * 100 - 1;
     
     if (document.getElementById("timeLine") !== null) {
         document.getElementById("timeLine").remove();
     }
-    if (document.getElementById("day" + day) !== null) {
+    if (document.getElementById("day" + data.day) !== null) {
         timeLine.style.top = dayPercentage + "%";
-        document.getElementById("day" + day).appendChild(timeLine);
+        document.getElementById("day" + data.day).appendChild(timeLine);
     }
 }, 1500);
